55bfeb93e6929f80f9acca72789ae3bc30babc6f
Byron Blocker <byronblocker@gmail.com>
2017-07-25 00:27:21 -0700

    Add project files.

---
+++ HelloVisual/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloVisual
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Visual Studio");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
